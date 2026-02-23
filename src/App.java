57a101472f0c203f2394d3fb5151a0c825823ec9
Egor-Vedenkin <egorushkavedenkin@yandex.ru>
2026-02-23 20:07:58 +0300

    clear

--- src/App.java
+++ src/App.java
@@ -1,22 +1,7 @@
-import org.skypro.skyshop.product.DiscountedProduct;
-import org.skypro.skyshop.product.FixPriceProduct;
-import org.skypro.skyshop.product.Product;
-import org.skypro.skyshop.basket.ProductBasket;
-import org.skypro.skyshop.product.SimpleProduct;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
     public static void main(String[] args) {
-        DiscountedProduct discountedPhone = new DiscountedProduct("Телефончик", 3000);
-        FixPriceProduct fixedNotebook = new FixPriceProduct("Фиксированный ноутбук");
-        SimpleProduct simpleTablet = new SimpleProduct("Обычный планшет", 5000);
-
-        ProductBasket basket = new ProductBasket();
-        basket.addToCart(discountedPhone);
-        basket.addToCart(fixedNotebook);
-        basket.addToCart(simpleTablet);
-
-        basket.showCart();
+        System.out.println("Hello World");
     }
 }
